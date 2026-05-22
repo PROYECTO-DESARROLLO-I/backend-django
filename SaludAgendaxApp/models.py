abce69f513a8e0aa50441479c88769e7a98e2c6b
sebastian <rubio.sebastian@correounivalle.edu.co>
2026-05-22 14:06:08 -0500

    creating initial models

--- SaludAgendaxApp/models.py
+++ SaludAgendaxApp/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
 # Create your models here.
+#
+
+
+class User(models.Model):
+    roles = [
+        "Patient",
+        "Doctor",
+        "Admin",
+        "SuperAdmin"
+    ]
+    name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=20, choices=roles)
+    active = models.BooleanField(default=False)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+
+class Patient(models.Model):
+    document_types = [
+        "CC",
+        "TI",
+        "RC",
+        "PPT",
+        "PEP"
+    ]
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    identity_document = models.CharField(unique=True)
+    document_type = models.CharField(choices=document_types)
+    date_birth = models.DateField()
+    phone_number = models.CharField(max_length=10)
+    address = models.TextField()
+
+
+class EPS(models.Model):
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=255)
+    active = models.BooleanField()
+
+
+class Doctor(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    eps_id = models.ForeingKey(EPS, on_delete=models.SET_NULL)
+    identity_document = models.CharField()
+    register_number = models.CharField()  # medic register number
+    phone_number = models.CharField()
+    active = models.BooleanField()
+
+
+class Headquarters(models.Model):
+    pass
+
+
+class Admin(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    headquarters_id = models.ForeignKey(Headquarters, on_delete=models.CASCADE)
+    # TODO: roles?
+
+
+class Specialties(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    active = models.BooleanField()
--- saludAgendaX/settings.py
+++ saludAgendaX/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'saludAgendaX.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'desarrollo1-db',
+         'USER': 'postgresql',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
